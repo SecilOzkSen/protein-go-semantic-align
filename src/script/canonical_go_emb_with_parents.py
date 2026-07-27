3cdafc71bad19508bf548b5061dae0a59623350e
SecilOzkSen <secilozkanoglu@gmail.com>
2026-07-27 09:21:44 +0300

    canonical go emb extraction for name definition only.

--- src/script/canonical_go_emb_with_parents.py
+++ src/script/canonical_go_emb_with_parents.py
@@ -263,11 +263,11 @@ def main(
 if __name__ == "__main__":
     main(
         go_path=(
-            "/workspace/data_pfresgo/processed/"
+            "/workspace/"
             "go_texts_canonical_segmented.jsonl"
         ),
         output_prefix=(
-            "/workspace/data_pfresgo/go_cache/all/"
+            "/workspace/go_cache/all/"
             "go_text_embeddings_canonical"
         ),
         phase="pfresgo_2020_06_01",
